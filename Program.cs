47a10f20eaddd6e18de2017dc4fd3b327a9dd85d
bosvs <eroinevs@gmail.com>
2024-12-12 01:34:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace FactoryMethod
+{
+    class FactoryMethod
+    {
+        static void Main(string[] args)
+        {
+
+            SenderFactory emailFactory = new EmailSenderFactory();
+            ISender emailSender = emailFactory.CreateSender();
+            emailSender.Send("Greetings via Email!");
+
+            SenderFactory smsFactory = new SmsSenderFactory();
+            ISender smsSender = smsFactory.CreateSender();
+            smsSender.Send("Greetings via SMS!");
+
+            Console.ReadLine();
+        }
+    }
+
+    interface ISender
+    {
+        void Send(string message);
+    }
+
+
+    class EmailSender : ISender
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"EmailSender: Dispatching email with content: {message}");
+        }
+    }
+
+
+    class SmsSender : ISender
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"SmsSender: Transmitting SMS with content: {message}");
+        }
+    }
+
+
+    abstract class SenderFactory
+    {
+        public abstract ISender CreateSender();
+    }
+
+
+    class EmailSenderFactory : SenderFactory
+    {
+        public override ISender CreateSender()
+        {
+            return new EmailSender();
+        }
+    }
+
+    class SmsSenderFactory : SenderFactory
+    {
+        public override ISender CreateSender()
+        {
+            return new SmsSender();
+        }
+    }
+}
